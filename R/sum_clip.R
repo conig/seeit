5dc045d6b59dcd028cec11c607c611c71564da10
conig <james.conigrave@gmail.com>
2023-02-08 13:56:53 +1100

    return_x arg added.

--- R/sum_clip.R
+++ R/sum_clip.R
@@ -2,11 +2,13 @@
 #'
 #' Sum values in a clipboard taken from excel'
 #' @param big.mark bool. If TRUE commas are inserted to differentiate long digits
+#' @param return_x bool. If you want to see the input vector
 #' @export
 
-sum_clip <- function(big.mark = TRUE){
+sum_clip <- function(big.mark = TRUE, return_x = FALSE){
 
-  x <- unlist(clipr::read_clip_tbl(header = FALSE))
+  x <- unname(unlist(clipr::read_clip_tbl(header = FALSE)))
+  if(return_x) return(x)
   x <- as.numeric(gsub("\\,", "", x))
   x <- sum(x)
 
